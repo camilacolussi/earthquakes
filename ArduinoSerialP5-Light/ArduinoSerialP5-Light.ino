9ef69e13d72ff92b5d48dba7b1a33b1b4c92407a
Camila Colussi <camilacolussicuadros@gmail.com>
2021-06-09 14:25:02 +0100

    add arduino file

--- ArduinoSerialP5-Light/ArduinoSerialP5-Light.ino
+++ ArduinoSerialP5-Light/ArduinoSerialP5-Light.ino
@@ -1,34 +1,13 @@
-//LED Strip
-#define FASTLED_ALLOW_INTERRUPTS 1
-#define FASTLED_INTERRUPT_RETRY_COUNT 1
-#include <FastLED.h>
-#define LED_PIN     9
-#define NUM_LEDS    65
-CRGB leds[NUM_LEDS];
-
 void setup() {
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  Serial.begin(9600);           // initialize serial communications
 }
-
+ 
 void loop() {
-int beam = 4; 
-int ledNum =   NUM_LEDS + beam;
-
-  
-  for (int i = 0; i <= ledNum; i++) {
-    leds[i] = CRGB(255, 255, 255);
-    leds[i-4] = CRGB(0, 0, 0);
-    FastLED.show();
-    delay(10);
+  if (Serial.available() > 0) { // if there's serial data available
+    int inByte = Serial.read();   // read it
+    Serial.write(inByte);         // send it back out as raw binary data
+    analogWrite(5, inByte);       // use it to set the LED brightness
+    // if you're using a speaker instead of an LED, uncomment line below  and comment out the previous line:
+    //  tone(5, inByte*10);     // play tone on pin 5
   }
-
-      
-//  if (Serial.available() > 0) {
-//   FastLED.clear();
-//   //Serial.begin(9600);
-////  // if there's serial data available
-////    int inByte = Serial.read();   // read it
-////    Serial.write(inByte);         // send it back out as raw binary data
-////    analogWrite(5, inByte);       // use it to set the LED brightness 
-//    } 
-  }
+}
